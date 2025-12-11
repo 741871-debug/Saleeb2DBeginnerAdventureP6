1ad020117f8d233e81e337f32f268c0276264b52
741871-debug <741871@student.dusd.net>
2025-12-10 16:27:30 -0800

    hi
    helloz

--- P6Saleeb2DBeginnerAdventure/P6SaleebAmir2DBeginnerAdventure/Assets/Scripts/PlayerControler.cs
+++ P6Saleeb2DBeginnerAdventure/P6SaleebAmir2DBeginnerAdventure/Assets/Scripts/PlayerControler.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.WSA;
 
 public class PlayerControler : MonoBehaviour
 {
     public float speed = 3.0f;
 
     public int maxhealth = 5;
+
+    public GameObject projectilePrefab;
+
     public float timeInvincible = 2;
 
     public int health { get { return currentHealth; } }
@@ -19,6 +24,10 @@ public class PlayerControler : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+    private object animator;
+
+   
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +35,7 @@ public class PlayerControler : MonoBehaviour
 
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxhealth;
-        currentHealth = 1;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,6 +43,15 @@ public class PlayerControler : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
 
         if (isinvincible)
         {
@@ -43,8 +61,23 @@ public class PlayerControler : MonoBehaviour
                 isinvincible = false;
             }
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+            
+        }
 
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+           
+        }
     }
+
+    private void Launch()
+    {
+        throw new NotImplementedException();
+    }
+
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -57,13 +90,28 @@ public class PlayerControler : MonoBehaviour
     {
         if(amount < 0)
         {
+            
+            if(isinvincible)
+            {
+                return;
+            }
             return;
         }
         isinvincible = true;
         invincibleTimer = timeInvincible;
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxhealth);
-        Debug.Log(currentHealth + "/" + maxhealth + (5));
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxhealth);
+    }
+
+    void Lunch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        
     }
 
 }
